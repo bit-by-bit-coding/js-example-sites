c60e3236027b3d7cbffb5ed92fc553b0eda95e1b
Selena <selenazhxng@gmail.com>
2020-08-08 15:11:23 -0700

    delete buttons

--- Full Game/script.js
+++ Full Game/script.js
@@ -1,3 +1,3 @@
 let message = document.getElementById("message");
-let button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
+
+message.innerText = "Hello!";
