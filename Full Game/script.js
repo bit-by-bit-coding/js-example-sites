24a0db186da3809476edc2980cc359596961a70c
Selena <selenazhxng@gmail.com>
2020-08-07 18:56:42 -0700

    add html and get elements in js

---
+++ Full Game/script.js
@@ -0,0 +1,4 @@
+let image = document.getElementById("image");
+let message = document.getElementById("message");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
