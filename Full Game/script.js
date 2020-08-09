80097d9b778e804d0308eef29ad4be9f799a11d2
Selena <selenazhxng@gmail.com>
2020-08-08 23:18:53 -0700

    add location/title element

--- Full Game/script.js
+++ Full Game/script.js
@@ -34,16 +34,16 @@ function play() {
         items[i].hasItem = false;
     }
     //go to woods
-    continueStory("Welcome, explorer! You're on a grand, perilous, magical quest to buy some delicious french fries. At the moment, however, you're stuck in some unfamiliar woods. Survive the wilderness and escape the forest first!", "Go Left", "Go Right", "CHEAT: Skip to village", goToRiver, goToCliff, goToVillage, "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    continueStory("Our Journey Begins...", "Welcome, explorer! You're on a grand, perilous, magical quest to buy some delicious french fries. At the moment, however, you're stuck in some unfamiliar woods. Survive the wilderness and escape the forest first!", "Go Left", "Go Right", "CHEAT: Skip to village", goToRiver, goToCliff, goToVillage, "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
 }
 
 //Locations
 function goToVillage() {
-    continueStory("You come to a bustling village.", "Go to the Market", "Walk around", "Go right, up a hill", goToMarket, goToOutskirts, goToCastle, "https://images.unsplash.com/photo-1508913950751-d1d139a29e68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    continueStory("Village Center", "You come to a bustling village.", "Go to the Market", "Walk around", "Go right, up a hill", goToMarket, goToOutskirts, goToCastle, "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fcb04791-9837-40cf-9484-bafcc8c45e72/d7ahq9v-e0609643-3743-4de1-ba49-54b95f0d90d8.jpg/v1/fill/w_1063,h_752,q_70,strp/palace_of_the_corsairs__harad_by_direimpulse_d7ahq9v-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0zNTA4IiwicGF0aCI6IlwvZlwvZmNiMDQ3OTEtOTgzNy00MGNmLTk0ODQtYmFmY2M4YzQ1ZTcyXC9kN2FocTl2LWUwNjA5NjQzLTM3NDMtNGRlMS1iYTQ5LTU0Yjk1ZjBkOTBkOC5qcGciLCJ3aWR0aCI6Ijw9NDk2MSJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.8k8Hmp3ze2NcXC_qNyReKFKbmkwdNcJWoepqgJ5C7H0");
 }
 
 function goToMarket() {
-    continueStory("Welcome to the market! You can buy and sell items here. Select whether you want to buy or sell first. Then type what you want to trade (either 'key', 'ruby', or 'a bag of french fries') into the input box and click Trade.", "Buy", "Sell", "Return to Village Center", buy, sell, goToVillage, "https://i.ytimg.com/vi/19dx6AkC_GY/maxresdefault.jpg");
+    continueStory("Market", "Welcome to the market! You can buy and sell items here. Select whether you want to buy or sell first. Then type what you want to trade (either 'key', 'ruby', or 'a bag of french fries') into the input box and click Trade.", "Buy", "Sell", "Return to Village Center", buy, sell, goToVillage, "https://i.ytimg.com/vi/19dx6AkC_GY/maxresdefault.jpg");
     showInput("Trade");
     submitButtonElement.onclick = () => {
         if (inputElement.value === "key") {
@@ -60,7 +60,7 @@ function goToMarket() {
 }
 
 function goToOutskirts() {
-    continueStory("You arrive at the outskirts of the village. There, several villagers", "Buy", "Sell", "Return to Village Center", goToTavern, goToVillage, goToCastle, "https://images.unsplash.com/photo-1591804860948-cdb450a32b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80");
+    continueStory("Village Outskirts", "You arrive at the outskirts of the village. There, several villagers", "Buy", "Sell", "Return to Village Center", goToTavern, goToVillage, goToCastle, "https://images.unsplash.com/photo-1591804860948-cdb450a32b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80");
     showInput("Calculate");
     submitButtonElement.onclick = () => {
         if (inputElement.value === "key") {
@@ -72,11 +72,11 @@ function goToOutskirts() {
 }
 
 function goToTavern() {
-    continueStory("You slip into a warm tavern. You notice several villagers playing dice in the corner.", "Gamble", "Get a drink", "Leave the tavern", gamble, buyDrink, goToVillage, "https://images.unsplash.com/photo-1591804860948-cdb450a32b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80");
+    continueStory("Tavern", "You slip into a warm tavern. You notice several villagers playing dice in the corner.", "Gamble", "Get a drink", "Leave the tavern", gamble, buyDrink, goToVillage, "https://images.unsplash.com/photo-1591804860948-cdb450a32b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80");
 }
 
 function gamble() {
-    continueStory("You decide to join the game of dice. Each die has twenty sides (you can roll a number between 1 and 20). If the two numbers on your dice are greater than 25, then each of the three villagers at the table will give you your bet. If you lose, you'll lose the money you betted. How much do you want to bet?", "Get a drink", "Leave the tavern", "Return to Village Center", buyDrink, goToTavern, goToVillage, "https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1265&q=80");
+    continueStory("Dice Minigame", "You decide to join the game of dice. Each die has twenty sides (you can roll a number between 1 and 20). If the two numbers on your dice are greater than 25, then each of the three villagers at the table will give you your bet. If you lose, you'll lose the money you betted. How much do you want to bet?", "Get a drink", "Leave the tavern", "Return to Village Center", buyDrink, goToTavern, goToVillage, "https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1265&q=80");
     showInput("Bet and Play");
     submitButtonElement.onclick = () => {
         let bet = inputElement.value;
@@ -103,11 +103,11 @@ function gamble() {
 
 function goToCastle() {
     items[1].hasItem = true;
-    continueStory("Above a hill sits a sprawling castle. You approach the gates in awe, until -- ouch -- you stub your toe. 'Fiddlesticks!' you say. Luckily, you stubbed your toe on a massive ruby, which you proceed to stuff in your pocket.", "Try the door", "Go down the hill to the village", "Die Randomly", tryDoor, goToVillage, die, "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6ff2ccfb-d302-4348-87bc-8b9568748e63/d7q7esw-bddb370b-31eb-401a-86eb-f68f437b69fe.jpg/v1/fill/w_723,h_1000,q_75,strp/sanctuary_by_jjcanvas_d7q7esw-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDAwIiwicGF0aCI6IlwvZlwvNmZmMmNjZmItZDMwMi00MzQ4LTg3YmMtOGI5NTY4NzQ4ZTYzXC9kN3E3ZXN3LWJkZGIzNzBiLTMxZWItNDAxYS04NmViLWY2OGY0MzdiNjlmZS5qcGciLCJ3aWR0aCI6Ijw9NzIzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.b52NvktP8FU_yXPDLPAvYapyKQma9VcKqsXjZbWEMf8");
+    continueStory("Castle Gate", "Above a hill sits a sprawling castle. You approach the gates in awe, until -- ouch -- you stub your toe. 'Fiddlesticks!' you say. Luckily, you stubbed your toe on a massive ruby, which you proceed to stuff in your pocket.", "Try the door", "Go down the hill to the village", "Die Randomly", tryDoor, goToVillage, die, "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6ff2ccfb-d302-4348-87bc-8b9568748e63/d7q7esw-bddb370b-31eb-401a-86eb-f68f437b69fe.jpg/v1/fill/w_723,h_1000,q_75,strp/sanctuary_by_jjcanvas_d7q7esw-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDAwIiwicGF0aCI6IlwvZlwvNmZmMmNjZmItZDMwMi00MzQ4LTg3YmMtOGI5NTY4NzQ4ZTYzXC9kN3E3ZXN3LWJkZGIzNzBiLTMxZWItNDAxYS04NmViLWY2OGY0MzdiNjlmZS5qcGciLCJ3aWR0aCI6Ijw9NzIzIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.b52NvktP8FU_yXPDLPAvYapyKQma9VcKqsXjZbWEMf8");
 }
 
 function goToTreasureRoom() {
-    continueStory("You found the treasure room! Small problem though: there's a very angry dragon sitting on top of all the gold.", "Run!", "Battle the Dragon!", "Eat Cheese", goToCastle, battle, eatCheese, "https://thumbs-prod.si-cdn.com/dDELtbwEtmDWEfjMw2HaDlU_5uQ=/fit-in/1600x0/filters:focal(949x245:950x246)/https://public-media.si-cdn.com/filer/6a/e6/6ae65dac-2e24-452d-a39e-0a6a7fb280f0/smaug_par_david_demaret.jpg");
+    continueStory("Treasure Room", "You found the treasure room! Small problem though: there's a very angry dragon sitting on top of all the gold.", "Run!", "Battle the Dragon!", "Eat Cheese", goToCastle, battle, eatCheese, "https://thumbs-prod.si-cdn.com/dDELtbwEtmDWEfjMw2HaDlU_5uQ=/fit-in/1600x0/filters:focal(949x245:950x246)/https://public-media.si-cdn.com/filer/6a/e6/6ae65dac-2e24-452d-a39e-0a6a7fb280f0/smaug_par_david_demaret.jpg");
 }
 
 // Transactions
@@ -162,21 +162,21 @@ function buyDrink() {
 
 function battle() {
     if (army > 30) {
-        continueStory("Your massive army stands behind you. All of them unsheathe their swords and wave them in the air, unintentionally revealing their armpits. The dragon wrinkles its nose at all that body odor. Dragons have a sensitive sense of smell, you know. Anyway, the dragon's gone, so you can take all the treasure now.", "Return to the village", "Share the treasure", "Die randomly", goToVillage, shareTreasure, die, "https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+        continueStory("Dragon's Lair", "Your massive army stands behind you. All of them unsheathe their swords and wave them in the air, unintentionally revealing their armpits. The dragon wrinkles its nose at all that body odor. Dragons have a sensitive sense of smell, you know. Anyway, the dragon's gone, so you can take all the treasure now.", "Return to the village", "Share the treasure", "Die randomly", goToVillage, shareTreasure, die, "https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
         coins += 10000;
         showInventory();
     } else {
-        badEnding("Unfortunately, the dragon was not in a good mood today. Try making more friends before you go around fighting dragons. Teamwork makes the dream work!", goToTreasureRoom, "https://www.rpnation.com/gallery/dragon-fire-android-wallpapers.26804/full?d=1494007413");
+        badEnding("The dragon burped...", "Unfortunately, the dragon was not in a good mood today. Try making more friends before you go around fighting dragons. Teamwork makes the dream work!", goToTreasureRoom, "https://www.rpnation.com/gallery/dragon-fire-android-wallpapers.26804/full?d=1494007413");
     }
 }
 
 // Transportation
 function goToRiver() {
-    continueStory("You find a rushing river. How will you cross it?", "Swim", "Boat", "Travel Interdimensionally", swim, goToBoat, interdimensionalTravel, "https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    continueStory("River", "You find a rushing river. How will you cross it?", "Swim", "Boat", "Travel Interdimensionally", swim, goToBoat, interdimensionalTravel, "https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
 }
 
 function goToBoat() {
-    continueStory("You hop in the boat floating by the bank and row over to the other side. You see houses through the trees in front of you. To the left of you is more forest and what looks like a stone tower.", "Go toward the houses", "Go toward the tower", "Swim back through the river", goToVillage, goToCastle, swim, "https://images.unsplash.com/photo-1527287993547-b5d3ad9ca875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1580&q=80");
+    continueStory("Out of the Wilderness!", "You hop in the boat floating by the bank and row over to the other side. You see houses through the trees in front of you. To the left of you is more forest and what looks like a stone tower.", "Go toward the houses", "Go toward the tower", "Swim back through the river", goToVillage, goToCastle, swim, "https://images.unsplash.com/photo-1527287993547-b5d3ad9ca875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1580&q=80");
 }
 
 function tryDoor() {
@@ -190,19 +190,19 @@ function tryDoor() {
 
 // Fatal choices
 function eatCheese() {
-    badEnding("You nervously reach in your pocket and start nibbling some cheese. The dragon proceeds to make you into a grilled cheese sandwich. Eating cheese solves many problems, but battling dragons is not one of them.", goToTreasureRoom, "https://images.unsplash.com/photo-1528736235302-52922df5c122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1644&q=80");
+    badEnding("Dragon's Belly", "You nervously reach in your pocket and start nibbling some cheese. The dragon proceeds to make you into a grilled cheese sandwich. Eating cheese solves many problems, but battling dragons is not one of them.", goToTreasureRoom, "https://images.unsplash.com/photo-1528736235302-52922df5c122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1644&q=80");
 }
 
 function interdimensionalTravel() {
-    badEnding("You get stuck in a universe with only one dimension. Not sure how you're supposed to go back to existing...", goToRiver, "https://images.unsplash.com/photo-1529651795107-e5a141e34843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80");
+    badEnding("One-Dimensional Space", "You get stuck in a universe with only one dimension. Not sure how you're supposed to go back to existing...", goToRiver, "https://images.unsplash.com/photo-1529651795107-e5a141e34843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80");
 }
 
 function swim() {
-    badEnding("You dive into the river, 10-foot tall waves frothing and storming around you! You battle couragenously through the turbulent whorls (conveniently ignoring how there was a literal boat next to you earlier). Finally, you feel the riverbed beneath your toes. You're two feet away from dry land when you step on something squishy. It's a sea slug! And...*gasp*...it's bright orange! You died of surprise. Try again?", goToRiver, "https://images.unsplash.com/photo-1518357019504-81a1bb8cda12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    badEnding("Started by Slugs", "You dive into the river, 10-foot tall waves frothing and storming around you! You battle couragenously through the turbulent whorls (conveniently ignoring how there was a literal boat next to you earlier). Finally, you feel the riverbed beneath your toes. You're two feet away from dry land when you step on something squishy. It's a sea slug! And...*gasp*...it's bright orange! You died of surprise. Try again?", goToRiver, "https://images.unsplash.com/photo-1518357019504-81a1bb8cda12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
 }
 
 function goToCliff() {
-    badEnding("You accidentally walked off a cliff and died. Oops. Keep your eyes on the road while wandering aimlessly in the wilderness :)", play, "https://images.unsplash.com/photo-1570877316396-0477e81e9d8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    badEnding("Cliff", "You accidentally walked off a cliff and died. Oops. Keep your eyes on the road while wandering aimlessly in the wilderness :)", play, "https://images.unsplash.com/photo-1570877316396-0477e81e9d8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
 }
 
 // Functions to keep the game going
@@ -210,8 +210,9 @@ function die() {
     changeButtons("Restart at the woods", "Restart at the woods", "Restart at the woods", play, play, play);
 }
 
-function continueStory(story, choice1, choice2, choice3, choice1Function, choice2Function, choice3Function, source) {
+function continueStory(location, story, choice1, choice2, choice3, choice1Function, choice2Function, choice3Function, source) {
     showInventory();
+    h1Element.innerText = location;
     inputElement.style.display = "none";
     submitButtonElement.style.display = "none";
     pElement.innerText = story;
@@ -219,10 +220,8 @@ function continueStory(story, choice1, choice2, choice3, choice1Function, choice
     imageElement.src = source;
 }
 
-function badEnding(story, lastLocation, source) {
-    imageElement.src = source;
-    pElement.innerText = story;
-    changeButtons("Try from last checkpoint", "Restart", "Restart", lastLocation, play, play);
+function badEnding(location, story, lastLocation, source) {
+    continueStory(location, story, "Try from last checkpoint", "Try from last checkpoint", "Restart", lastLocation, lastLocation, play, source);
 }
 
 function changeButtons(choice1, choice2, choice3, choice1Function, choice2Function, choice3Function) {
